09245e35e26bde38ed61475cf01c3b1113e7b4a5
aromazanov <aromazanov@users.noreply.github.com>
2025-04-16 11:58:15 +0200

    Hate speech front end handling added.

--- delab_web/front_end/django_chatbot/chatbot/views.py
+++ delab_web/front_end/django_chatbot/chatbot/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-# Create your views here.
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
